a025dfe4913c4d4aa4b1979462a3f35c973ae0a4
ykhwong <ykhwong@daum.net>
2018-08-16 13:04:43 +0900

    [FIX] Quotes can be handled now

--- preprocess.pl
+++ preprocess.pl
@@ -2,27 +2,29 @@ sub main {
 	my %unique_import_id;
 	while (<>) {
 		my $ls = $_;
-		next if ($ls =~ /^SCHEMA AWAIT/i);
-		next if ($ls =~ /^(CREATE|DROP) +CONSTRAINT .+UNIQUE IMPORT/i);
+		next if ($ls =~ /^SCHEMA +AWAIT/i);
+		next if ($ls =~ /^(CREATE|DROP) +CONSTRAINT .+UNIQUE +IMPORT/i);
 		next if ($ls =~ /^MATCH .+ REMOVE .+/i);
 
 		# Basic change
-		$ls =~ s/(`|")/'/g;
+		$ls =~ s/'/''/g;
+		$ls =~ s/([^\\])(`|")/$1'/g;
+		$ls =~ s/\\"/"/g;
 		$ls =~ s/^\s*BEGIN\s*$/BEGIN;/i;
 		$ls =~ s/^\s*COMMIT\s*$/COMMIT;/i;
 
-		if ($ls =~ /CREATE \(:'(\S+)':'UNIQUE IMPORT LABEL' \{(.+), 'UNIQUE IMPORT ID':(\d+)\}\);/i) {
+		if ($ls =~ /CREATE +\(:'(\S+)':'UNIQUE +IMPORT +LABEL' +\{(.+), 'UNIQUE +IMPORT +ID':(\d+)\}\);/i) {
 			my $vlabel = $1;
 			my $keyval = $2;
 			my $id = $3;
 			$unique_import_id{$id} = $vlabel . "\t" . $keyval;
-			$ls =~ s/CREATE \(:'(\S+)':'UNIQUE IMPORT LABEL' \{/CREATE (:$1 {/i;
-			$ls =~ s/, 'UNIQUE IMPORT ID':\d+\}/\}/i;
+			$ls =~ s/CREATE +\(:'(\S+)':'UNIQUE +IMPORT +LABEL' +\{/CREATE (:$1 {/i;
+			$ls =~ s/, +'UNIQUE +IMPORT +ID':\d+\}/\}/i;
 		}
-		if ($ls =~ /^MATCH \(n1:'UNIQUE IMPORT LABEL'(\{'UNIQUE IMPORT ID':\d+\})\), +\(n2:'UNIQUE IMPORT LABEL'(\{'UNIQUE IMPORT ID':\d+\})\)/i) {
+		if ($ls =~ /^MATCH +\(n1:'UNIQUE +IMPORT +LABEL'(\{'UNIQUE +IMPORT +ID':\d+\})\), +\(n2:'UNIQUE +IMPORT +LABEL'(\{'UNIQUE +IMPORT +ID':\d+\})\)/i) {
 			my $n1 = $1;
 			my $n2 = $2;
-			$ls =~ s/'UNIQUE IMPORT LABEL'//ig;
+			$ls =~ s/'UNIQUE +IMPORT +LABEL'//ig;
 			$ls =~ s/\[r:'(\S+)'\]/[r:$1]/i;
 			$ls =~ s/\[:'(\S+)'\]/[:$1]/i;
 			if ($n1 =~ /(\d+)/) {
@@ -38,19 +40,19 @@ sub main {
 				$ls =~ s/$n2/$id/i;
 			}
 		}
-		if ($ls =~ /^CREATE \(:'(\S+)'/i) {
-			$ls =~ s/^CREATE \(:'(\S+)'/CREATE (:$1/i;
+		if ($ls =~ /^CREATE +\(:'(\S+)'/i) {
+			$ls =~ s/^CREATE +\(:'(\S+)'/CREATE +(:$1/i;
 		}
-		if ($ls =~ /^CREATE INDEX ON :/i) {
-			$ls =~ s/^CREATE INDEX ON :/CREATE PROPERTY INDEX ON /i;
+		if ($ls =~ /^CREATE +INDEX +ON +:/i) {
+			$ls =~ s/^CREATE +INDEX +ON +:/CREATE PROPERTY INDEX ON /i;
 			$ls =~ s/'//g;
 		}
-		if ($ls =~ /^CREATE CONSTRAINT ON \(\S+:'(\S+)'\) ASSERT \S+\.'(\S+)'/i) {
-			$ls =~ s/^CREATE CONSTRAINT ON \(\S+:'(\S+)'\) ASSERT \S+\.'(\S+)'/CREATE CONSTRAINT ON $1 ASSERT $2/i;
+		if ($ls =~ /^CREATE +CONSTRAINT +ON +\(\S+:'(\S+)'\) +ASSERT +\S+\.'(\S+)'/i) {
+			$ls =~ s/^CREATE +CONSTRAINT +ON +\(\S+:'(\S+)'\) +ASSERT +\S+\.'(\S+)'/CREATE CONSTRAINT ON $1 ASSERT $2/i;
 		}
-		if ($ls =~ /^MATCH \(n1:'(\S+)'/i) {
-			$ls =~ s/^MATCH \(n1:'(\S+)'\s*\{/MATCH (n1:$1 {/i;
-			$ls =~ s/ \(n2:'(\S+)'\s*\{/ (n2:$1 {/i;
+		if ($ls =~ /^MATCH +\(n1:'(\S+)'/i) {
+			$ls =~ s/^MATCH +\(n1:'(\S+)'\s*\{/MATCH (n1:$1 {/i;
+			$ls =~ s/ +\(n2:'(\S+)'\s*\{/ (n2:$1 {/i;
 			$ls =~ s/\[:'(\S+)'\]/[:$1]/i;
 		}
 
