fe59fb704b634a2d5613e81c59212c95fd44fbe2
ykhwong <ykhwong@daum.net>
2018-08-16 14:36:31 +0900

    [FIX] Make codes pretty

--- preprocess.pl
+++ preprocess.pl
@@ -1,5 +1,7 @@
 sub main {
 	my %unique_import_id;
+	my $UIL="'UNIQUE +IMPORT +LABEL'";
+	my $UII="'UNIQUE +IMPORT +ID'";
 	while (<>) {
 		my $ls = $_;
 		next if ($ls =~ /^SCHEMA +AWAIT/i);
@@ -14,18 +16,18 @@ sub main {
 		$ls =~ s/^\s*BEGIN\s*$/BEGIN;/i;
 		$ls =~ s/^\s*COMMIT\s*$/COMMIT;/i;
 
-		if ($ls =~ /CREATE +\(:'(\S+)':'UNIQUE +IMPORT +LABEL' +\{(.+), 'UNIQUE +IMPORT +ID':(\d+)\}\);/i) {
+		if ($ls =~ /CREATE +\(:'(\S+)':$UIL +\{(.+), $UII:(\d+)\}\);/i) {
 			my $vlabel = $1;
 			my $keyval = $2;
 			my $id = $3;
 			$unique_import_id{$id} = $vlabel . "\t" . $keyval;
-			$ls =~ s/CREATE +\(:'(\S+)':'UNIQUE +IMPORT +LABEL' +\{/CREATE (:$1 {/i;
-			$ls =~ s/, +'UNIQUE +IMPORT +ID':\d+\}/\}/i;
+			$ls =~ s/CREATE +\(:'(\S+)':$UIL +\{/CREATE (:$1 {/i;
+			$ls =~ s/, +$UII:\d+\}/\}/i;
 		}
-		if ($ls =~ /^MATCH +\(n1:'UNIQUE +IMPORT +LABEL'(\{'UNIQUE +IMPORT +ID':\d+\})\), +\(n2:'UNIQUE +IMPORT +LABEL'(\{'UNIQUE +IMPORT +ID':\d+\})\)/i) {
+		if ($ls =~ /^MATCH +\(n1:$UIL(\{$UII:\d+\})\), +\(n2:$UIL(\{$UII:\d+\})\)/i) {
 			my $n1 = $1;
 			my $n2 = $2;
-			$ls =~ s/'UNIQUE +IMPORT +LABEL'//ig;
+			$ls =~ s/$UIL//ig;
 			$ls =~ s/\[r:'(\S+)'\]/[r:$1]/i;
 			$ls =~ s/\[:'(\S+)'\]/[:$1]/i;
 			if ($n1 =~ /(\d+)/) {
